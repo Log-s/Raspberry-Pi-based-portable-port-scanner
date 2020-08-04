48769313adba30d2ef8c9233306bb55cfb0aa1c0
Leo <leo9512906@gmail.com>
2020-08-04 18:23:21 +0200

    .

--- DjangoServer/data_visualization/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DataVisualizationConfig(AppConfig):
-    name = 'data_visualization'
--- data_visualization/server.py
+++
@@ -1,7 +0,0 @@
-from os import walk
-
-# get a list of all available scan reports
-
-reports = walk("../scanReports/")
-
-print(walk)
